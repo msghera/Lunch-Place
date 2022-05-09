6aec591fa2ca1876326ad101311b8959872172af
Ghazanfar <skghazanfar@gmail.com>
2022-05-10 01:39:06 +0600

    logging object added

--- lunch_place/app_logging.py
+++ lunch_place/app_logging.py
@@ -1,3 +1,9 @@
 import logging
+format = "%(asctime)s — %(levelname)s — %(funcName)s:%(lineno)d — %(message)s"
+logging.basicConfig(
+    format=format,
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level='INFO'
+)
 
 logger = logging.getLogger(__name__)
